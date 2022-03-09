83094e48bfb64fff2f71503eb4499ea6c18391c4
Igor Batko <igor.batko.batko@gmail.com>
2022-03-09 14:04:28 +0100

    Exercise 11.7

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
+
+import styles from './ColumnForm.module.scss';
+
+const ColumnForm = props => {
+
+    const [title, setTitle] = useState('')
+    const [icon, setIcon] = useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.action({ title: title, icon: icon });
+        setTitle('');
+        setIcon('');
+    };
+
+    return (
+        <form className={styles.columnForm} onSubmit={handleSubmit}>
+            <div className={styles.inputWrapper}>
+                <span className={styles.title}>Title:</span>
+                <TextInput value={title} onChange={e => setTitle(e.target.value)} />
+            </div>
+            <div className={styles.inputWrapper}>
+                <span className={styles.title}>Icon:</span>
+                <TextInput value={icon} onChange={e => setIcon(e.target.value)} />
+            </div>
+            <Button>Add column</Button>
+        </form>
+    );
+};
+
+export default ColumnForm;
