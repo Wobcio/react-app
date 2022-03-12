bea415dae0d808fc0221b1e105c88f47f719fb5c
Igor Batko <igor.batko.batko@gmail.com>
2022-03-12 12:24:47 +0100

    Exercise 13.2

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,14 @@
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
+
+
 import styles from './Column.module.scss';
 
 const Column = props =>{
+
+    const cards = useSelector(state => state.cards).filter(card => card.columnId === props.id)
+
     return (
         <article className={styles.column}>
                 <h2 className={styles.title}>
@@ -10,7 +16,7 @@ const Column = props =>{
                     {props.title}
                 </h2>
                 <ul className={styles.cards}>
-                    {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+                    {cards.map(card => <Card key={card.id} title={card.title} />)}
                 </ul>
                 <CardForm action={props.action} columnId={props.id} />
             </article>
